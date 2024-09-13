86a6903a66f50c0f54ea90d11038d350b0fe15cf
Mark Allman <mark@tomarq.co.uk>
2024-09-13 13:25:11 +0100

    Work in progress

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Ghost\GovUkFrontendBundle\GhostGovUkFrontendBundle::class => ['all' => true],
 ];
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
-const Encore = require('@symfony/webpack-encore');
+const Encore = require('@symfony/webpack-encore')
+const path = require("path");
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
-    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev')
 }
 
 Encore
@@ -44,17 +45,19 @@ Encore
 
     // configure Babel
     // .configureBabel((config) => {
-    //     config.plugins.push('@babel/a-babel-plugin');
+    //     config.plugins.push('@babel/a-babel-plugin')
     // })
 
     // enables and configure @babel/preset-env polyfills
     .configureBabelPresetEnv((config) => {
-        config.useBuiltIns = 'usage';
-        config.corejs = '3.23';
+        config.useBuiltIns = 'usage'
+        config.corejs = '3.23'
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader(function(options) {
+        options.sassOptions['quietDeps'] = true // Don't show deprecation warnings that originate from inside node_modules/
+    })
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
@@ -68,6 +71,21 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
-;
 
-module.exports = Encore.getWebpackConfig();
+let config = Encore.getWebpackConfig()
+
+let govukAssetsPath = path.resolve('./node_modules/govuk-frontend/dist/govuk/assets/')
+
+for(let rule of config.module.rules) {
+    if (rule.test.toString().match(/s\[ac]ss/)) {
+        for (let one of rule.oneOf) {
+            for (let tool of one.use) {
+                if (tool.loader.includes('/sass-loader/')) {
+                    tool.options.additionalData = '$govuk-assets-path: "' + govukAssetsPath + '/";'
+                }
+            }
+        }
+    }
+}
+
+module.exports = config
