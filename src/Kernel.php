e56a935681c8a1073119fc96c88b61838c53f586
Mark Allman <mark@tomarq.co.uk>
2024-11-04 10:39:47 +0000

    GCP deployment

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,10 +2,25 @@
 
 namespace App;
 
+use Ghost\GovUkCoreBundle\Features;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function getCacheDir(): string
+    {
+        return Features::isEnabled(Features::GAE_ENVIRONMENT)
+            ? sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'symf-cache'
+            : parent::getCacheDir();
+    }
+
+    public function getLogDir(): string
+    {
+        return Features::isEnabled(Features::GAE_ENVIRONMENT)
+            ? sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'symf-log'
+            : parent::getLogDir();
+    }
 }
