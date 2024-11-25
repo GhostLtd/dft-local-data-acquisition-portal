af9ef34ad1a8bd8b900a7395aeeca4214890720c
Mark Allman <mark@tomarq.co.uk>
2024-11-25 14:42:44 +0000

    Project -> Project elements

--- src/Entity/Enum/ProjectLevelSection.php
+++ src/Entity/Enum/ProjectLevelSection.php
@@ -3,6 +3,7 @@
 namespace App\Entity\Enum;
 
 use App\Form\ProjectReturn\Crsts\ProjectDetailsType;
+use App\Form\ProjectReturn\Crsts\ProjectElementsType;
 use App\Form\ProjectReturn\Crsts\ProjectTransportModeType;
 
 // N.B. Not used in the database, but rather to key slugs + translation keys for project-level returns
@@ -10,13 +11,15 @@ enum ProjectLevelSection: string
 {
     case PROJECT_DETAILS = 'project_details'; // e.g. name, description, id, retained, cdel/rdel
     case TRANSPORT_MODE = 'transport_mode';
+    case PROJECT_ELEMENTS = 'project_elements'; // active travel, charging points, clean air elements
 
     public static function getFormClassForFundAndSection(Fund $fund, ProjectLevelSection $section): ?string
     {
         return match($fund) {
             Fund::CRSTS => match($section) {
                 self::PROJECT_DETAILS => ProjectDetailsType::class,
-                self::TRANSPORT_MODE => ProjectTransportModeType::class
+                self::TRANSPORT_MODE => ProjectTransportModeType::class,
+                self::PROJECT_ELEMENTS => ProjectElementsType::class,
             },
             default => null,
         };
@@ -25,7 +28,7 @@ enum ProjectLevelSection: string
     public static function filterForFund(Fund $fund): array
     {
         return match($fund) {
-            Fund::CRSTS => [self::PROJECT_DETAILS, self::TRANSPORT_MODE],
+            Fund::CRSTS => [self::PROJECT_DETAILS, self::TRANSPORT_MODE, self::PROJECT_ELEMENTS],
             Fund::BSIP => throw new \RuntimeException('Not yet supported'),
         };
     }
---
+++ src/Form/ProjectReturn/Crsts/ProjectElementsType.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Form\ProjectReturn\Crsts;
+
+use App\Entity\Enum\ActiveTravelElement;
+use App\Entity\ProjectFund\ProjectFund;
+use App\Form\ReturnBaseType;
+use Ghost\GovUkFrontendBundle\Form\Type\BooleanChoiceType;
+use Ghost\GovUkFrontendBundle\Form\Type\ChoiceType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\DataMapperInterface;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProjectElementsType extends AbstractType implements DataMapperInterface
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->setDataMapper($this)
+            ->add('hasActiveTravelElements', BooleanChoiceType::class, [
+                'label' => 'forms.project.project_elements.has_active_travel_elements.label',
+                'label_attr' => ['class' => 'govuk-fieldset__legend--s'],
+                'help' => 'forms.project.project_elements.has_active_travel_elements.help',
+                'choice_options' => [
+                    'boolean.true' => [
+                        'conditional_form_name' => 'activeTravelElement',
+                    ]
+                ],
+            ])
+            ->add('activeTravelElement', ChoiceType::class, [
+                'label' => 'forms.project.project_elements.active_travel_element.label',
+                'label_attr' => ['class' => 'govuk-fieldset__legend--s'],
+                'help' => 'forms.project.project_elements.active_travel_element.help',
+                'choices' => ActiveTravelElement::cases(),
+                'choice_label' => fn(ActiveTravelElement $choice) => "enum.active_travel_element.{$choice->value}",
+                'choice_value' => fn(?ActiveTravelElement $choice) => $choice?->value,
+            ])
+            ->add('includesChargingPoints', BooleanChoiceType::class, [
+                'label' => 'forms.project.project_elements.includes_charging_points.label',
+                'label_attr' => ['class' => 'govuk-fieldset__legend--s'],
+                'help' => 'forms.project.project_elements.includes_charging_points.help',
+            ])
+            ->add('includesCleanAirElements', BooleanChoiceType::class, [
+                'label' => 'forms.project.project_elements.includes_clean_air_elements.label',
+                'label_attr' => ['class' => 'govuk-fieldset__legend--s'],
+                'help' => 'forms.project.project_elements.includes_clean_air_elements.help',
+            ]);
+    }
+
+    public function getParent(): string
+    {
+        return ReturnBaseType::class;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefault('data_class', ProjectFund::class);
+    }
+
+    public function mapDataToForms(mixed $viewData, \Traversable $forms): void
+    {
+        if (!$viewData instanceof ProjectFund) {
+            return;
+        }
+
+        $forms = iterator_to_array($forms);
+        /** @var FormInterface[] $forms */
+
+        $project = $viewData->getProject();
+        $activeTravelElement = $project->getActiveTravelElement();
+
+        $forms['hasActiveTravelElements']->setData($activeTravelElement !== null);
+        $forms['activeTravelElement']->setData($activeTravelElement);
+        $forms['includesChargingPoints']->setData($project->includesChargingPoints());
+        $forms['includesCleanAirElements']->setData($project->includesCleanAirElements());
+    }
+
+    public function mapFormsToData(\Traversable $forms, mixed &$viewData): void
+    {
+        if (!$viewData instanceof ProjectFund) {
+            return;
+        }
+
+        $forms = iterator_to_array($forms);
+        /** @var FormInterface[] $forms */
+
+        $project = $viewData->getProject();
+        $activeTravelElement = $forms['hasActiveTravelElements']->getData() ?
+            $forms['activeTravelElement']->getData() :
+            null;
+
+        $project
+            ->setActiveTravelElement($activeTravelElement)
+            ->setIncludesChargingPoints($forms['includesChargingPoints']->getData())
+            ->setIncludesCleanAirElements($forms['includesCleanAirElements']->getData());
+    }
+}
