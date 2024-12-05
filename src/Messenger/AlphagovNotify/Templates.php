b5ea7a49c638b9acc8f95aa5691e6b8bbc93380c
Mark Allman <mark@tomarq.co.uk>
2024-12-05 13:57:46 +0000

    Add login link template ID

--- src/Messenger/AlphagovNotify/Templates.php
+++ src/Messenger/AlphagovNotify/Templates.php
@@ -4,5 +4,5 @@ namespace App\Messenger\AlphagovNotify;
 
 class Templates
 {
-    public const string LOGIN_LINK = 'aaaaa-11111-bbbbb-222222';
+    public const string LOGIN_LINK = 'e891bc68-4a01-41c6-aa1f-6314df610057';
 }
