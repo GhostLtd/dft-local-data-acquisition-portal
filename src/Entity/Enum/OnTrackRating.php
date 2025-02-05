6f9c389150637327f6dbfb127d40a488b1e9948c
Mark Allman <mark@tomarq.co.uk>
2025-02-05 10:27:09 +0000

    Adjust scheme milestone. reduce list to: green, amber, red, completed, cancelled, split, merged [https://trello.com/c/aqoCzK14]

--- src/Entity/Enum/OnTrackRating.php
+++ src/Entity/Enum/OnTrackRating.php
@@ -5,28 +5,24 @@ namespace App\Entity\Enum;
 enum OnTrackRating: string
 {
     case GREEN = "green";
-    case GREEN_AMBER = "green_amber";
     case AMBER = "amber";
-    case AMBER_RED = "amber_red";
     case RED = "red";
     case SCHEME_COMPLETED = "scheme_completed";
-    case SCHEME_ON_HOLD = "scheme_on_hold";
-    case SCHEME_COMPLETED_LATE = "scheme_completed_late";
     case SCHEME_CANCELLED = "scheme_cancelled";
-    case SCHEME_NA = "not_applicable";
+    case SCHEME_SPLIT = "scheme_split";
+    case SCHEME_MERGED = "scheme_merged";
 
     public function getTagClass(): string
     {
         return 'govuk-tag--' . match ($this) {
-                self::RED => 'red',
-                self::AMBER_RED => 'pink',
-                self::AMBER => 'orange',
-                self::GREEN_AMBER => 'yellow',
-                self::GREEN => 'green',
+            self::RED => 'red',
+            self::AMBER => 'orange',
+            self::GREEN => 'green',
 
-                self::SCHEME_COMPLETED_LATE, self::SCHEME_COMPLETED => 'blue',
-
-                default => 'grey',
-            };
+            self::SCHEME_COMPLETED,
+            self::SCHEME_CANCELLED,
+            self::SCHEME_MERGED,
+            self::SCHEME_SPLIT => 'blue',
+        };
     }
 }
