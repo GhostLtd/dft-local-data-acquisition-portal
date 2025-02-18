257de54eed9954b1a1aa8df1c4c32fc80951f3db
Tom Sykes <tom@tomarq.co.uk>
2025-02-18 14:14:09 +0000

    admin (wip): - use entity type for admin when editing authority

--- src/Form/Type/Admin/AuthorityType.php
+++ src/Form/Type/Admin/AuthorityType.php
@@ -3,10 +3,14 @@
 namespace App\Form\Type\Admin;
 
 use App\Entity\Authority;
+use App\Entity\User;
 use App\Form\Type\BaseButtonsFormType;
 use Ghost\GovUkFrontendBundle\Form\Type as Gds;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AuthorityType extends AbstractType
@@ -19,10 +23,25 @@ class AuthorityType extends AbstractType
                 'label' => 'authority.form.name',
                 'label_attr' => ['class' => 'govuk-label--m'],
             ])
-            ->add('admin', UserType::class, [
-                'label' => 'authority.form.admin',
-                'label_attr' => ['class' => 'govuk-label--m'],
-            ])
+            ->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+                /** @var Authority $data */
+                $data = $event->getData();
+                $form = $event->getForm();
+                if ($data->getId()) {
+                    $form->add('admin', Gds\EntityType::class, [
+                        'class' => User::class,
+                        'choice_label' => 'name',
+                        'label' => 'authority.form.admin',
+                        'label_attr' => ['class' => 'govuk-label--m'],
+                    ]);
+                } else {
+                    $form->add('admin', UserType::class, [
+                        'label' => 'authority.form.admin',
+                        'label_attr' => ['class' => 'govuk-label--m'],
+                    ]);
+                }
+
+            })
             ;
     }
 
