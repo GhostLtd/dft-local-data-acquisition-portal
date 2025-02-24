74533b5185a9df814206d07e610a042f7841ed28
Tom Sykes <tom@tomarq.co.uk>
2025-02-24 12:49:38 +0000

    general: - don't save keys file to git - move GCP specific coonfig to seperate environment overlay (import manually in kernel) - move faker to prod requirements

--- src/Kernel.php
+++ src/Kernel.php
@@ -3,11 +3,35 @@
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    private function configureContainer(ContainerConfigurator $container, LoaderInterface $loader, ContainerBuilder $builder): void
+    {
+        $configDir = $this->getConfigDir();
+
+        $container->import($configDir.'/{packages}/*.{php,yaml}');
+        $container->import($configDir.'/{packages}/'.$this->environment.'/*.{php,yaml}');
+
+        if (Features::isEnabled(Features::GAE_ENVIRONMENT)) {
+            $container->import('../config/{packages}/gae/*.yaml');
+        }
+
+        if (is_file($configDir.'/services.yaml')) {
+            $container->import($configDir.'/services.yaml');
+            $container->import($configDir.'/{services}_'.$this->environment.'.yaml');
+        } else {
+            $container->import($configDir.'/{services}.php');
+            $container->import($configDir.'/{services}_'.$this->environment.'.php');
+        }
+    }
+
 
     public function getCacheDir(): string
     {
